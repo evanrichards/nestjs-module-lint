e417b21639d414bef0c5c2edfbb457bcca4375ec
Evan Richards <tevanrichards@gmail.com>
2024-05-14 00:04:51 -0700

    improve tsconfig parsing and path resolution

--- internal/path-resolver/path-resolver.go
+++ internal/path-resolver/path-resolver.go
@@ -2,15 +2,16 @@ package pathresolver
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 )
 
 type TsPathResolver struct {
-	tsConfigPath string
-	paths        map[string][]string
-	projectRoot  string
+	paths       map[string][]string
+	projectRoot string
 }
 
 type CompilerOptions struct {
@@ -23,8 +24,18 @@ type TsConfig struct {
 
 func removeCommentLinesFromJson(tsConfigFileContents []byte) []byte {
 	var tsConfigFileContentsWithoutComments []byte
+	inMultiLineComment := false
 	for _, line := range strings.Split(string(tsConfigFileContents), "\n") {
-		if !strings.HasPrefix(strings.TrimSpace(line), "//") {
+		trimmedLine := strings.TrimSpace(line)
+		if strings.HasPrefix(trimmedLine, "/*") {
+			inMultiLineComment = true
+			continue
+		}
+		if strings.HasSuffix(trimmedLine, "*/") {
+			inMultiLineComment = false
+			continue
+		}
+		if !inMultiLineComment && !strings.HasPrefix(trimmedLine, "//") {
 			tsConfigFileContentsWithoutComments = append(tsConfigFileContentsWithoutComments, line...)
 		}
 	}
@@ -32,40 +43,51 @@ func removeCommentLinesFromJson(tsConfigFileContents []byte) []byte {
 }
 
 func ParseTsConfigFile(tsConfigFileContents []byte) (*TsConfig, error) {
-	var tsConfig *TsConfig
-	err := json.Unmarshal(removeCommentLinesFromJson(
-		tsConfigFileContents), &tsConfig)
+	var tsConfig TsConfig
+	err := json.Unmarshal(removeCommentLinesFromJson(tsConfigFileContents), &tsConfig)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to unmarshal tsconfig file: %w", err)
 	}
-	return tsConfig, nil
-
+	return &tsConfig, nil
 }
 
-func NewTsPathResolverFromPath(tsConfigPath string) (*TsPathResolver, error) {
-	tsConfigFileContents, err := os.ReadFile(tsConfigPath)
+func NewTsPathResolverFromPath(projectRoot string) (*TsPathResolver, error) {
+	tsConfigFileContents, err := os.ReadFile(filepath.Join(projectRoot, "tsconfig.json"))
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not read tsconfig file: %w", err)
 	}
-	return NewTsPathResovler(tsConfigFileContents, tsConfigPath, filepath.Dir(tsConfigPath))
+	return NewTsPathResolver(tsConfigFileContents, projectRoot)
 }
 
-func NewTsPathResovler(tsConfigFileContents []byte, tsConfigPath string, projectRoot string) (*TsPathResolver, error) {
+func NewTsPathResolver(tsConfigFileContents []byte, projectRoot string) (*TsPathResolver, error) {
 	tsConfig, err := ParseTsConfigFile(tsConfigFileContents)
 	if err != nil {
 		return nil, err
 	}
 	return &TsPathResolver{
-		tsConfigPath: tsConfigPath,
-		paths:        tsConfig.CompilerOptions.Paths,
-		projectRoot:  projectRoot,
+		paths:       tsConfig.CompilerOptions.Paths,
+		projectRoot: projectRoot,
 	}, nil
 }
 
 func (t *TsPathResolver) ResolveImportPath(importPath string) string {
-	for key, value := range t.paths {
-		if strings.HasPrefix(importPath, key) {
-			return filepath.Join(t.projectRoot, value[0], strings.TrimPrefix(importPath, key))
+	if !strings.HasSuffix(importPath, ".ts") {
+		importPath = importPath + ".ts"
+	}
+
+	for alias, paths := range t.paths {
+		aliasPattern := "^" + strings.ReplaceAll(alias, "*", "(.*)") + "$"
+		regexpAlias := regexp.MustCompile(aliasPattern)
+		if regexpAlias.MatchString(importPath) {
+			submatches := regexpAlias.FindStringSubmatch(importPath)
+			for _, path := range paths {
+				resolvedPath := path
+				if strings.Contains(path, "*") && len(submatches) > 1 {
+					resolvedPath = strings.Replace(path, "*", submatches[1], 1)
+				}
+				absolutePath := filepath.Join(t.projectRoot, resolvedPath)
+				return absolutePath
+			}
 		}
 	}
 	return filepath.Join(t.projectRoot, importPath)
--- internal/path-resolver/path-resolver_test.go
+++ internal/path-resolver/path-resolver_test.go
@@ -1,96 +1,40 @@
 package pathresolver_test
 
 import (
-	"log"
 	"testing"
 
 	pathresolver "github.com/loop-payments/nestjs-module-lint/internal/path-resolver"
 )
 
-var tsConfigFile = `
-{
-  "extends": "@tsconfig/node20/tsconfig.json",
-  "compilerOptions": {
-    "module": "CommonJS",
-    "moduleResolution": "node10",
-    "lib": ["es2023", "esnext.disposable"],
-    "declaration": true,
-    "removeComments": true,
-    "emitDecoratorMetadata": true,
-    "experimentalDecorators": true,
-    "allowSyntheticDefaultImports": true,
-    "sourceMap": true,
-    "outDir": "./dist/src",
-    "baseUrl": "./",
-    "jsx": "react-jsx",
-    "strict": true,
-    "incremental": true,
-    "skipLibCheck": true,
-    "noImplicitAny": true,
-    // make DTO classes easier to write
-    "strictPropertyInitialization": false,
-    "forceConsistentCasingInFileNames": true,
-    "noFallthroughCasesInSwitch": true,
-    "useDefineForClassFields": false,
-    "paths": {
-      "src/*": ["./src/*"],
-      "@testing": ["./test/for/stuff"]
-    },
-    "resolveJsonModule": true
-  },
-  "references": [
-    { "path": "lib/email-templates" },
-    { "path": "lib/csv-templates" },
-    { "path": "lib/pdf-templates" }
-  ],
-  "ts-node": {
-    "swc": true
-  },
-  "watchOptions": {
-    "excludeFiles": ["src/common/util/test-util/*"]
-  }
-}
-
-`
-
-func TestParseTsConfig(t *testing.T) {
-	// Example TypeScript source to parse
-	parsed, err := pathresolver.ParseTsConfigFile([]byte(tsConfigFile))
-	if err != nil {
-		log.Fatalf("Failed to parse tsconfig file: %v", err)
-	}
-	if parsed == nil {
-		t.Fatalf("Failed to parse tsconfig file")
-	}
-	paths := parsed.CompilerOptions.Paths
-	if len(paths) != 2 {
-		t.Fatalf("Failed to parse paths")
-	}
-	if len(paths["src/*"]) != 1 {
-		t.Fatalf("Failed to parse paths")
-	}
-	if len(paths["@testing"]) != 1 {
-		t.Fatalf("Failed to parse paths")
-	}
-
-}
-
-func TestTsPathResovler(t *testing.T) {
-	tsPathResolver, err := pathresolver.NewTsPathResovler(
-		[]byte(tsConfigFile), "tsconfig.json", "/path/to/project")
-	if err != nil {
-		log.Fatalf("Failed to create ts path resolver: %v", err)
-	}
-	t.Run("resolve @paths", func(t *testing.T) {
-		path := tsPathResolver.ResolveImportPath("@testing/my-new-test")
-		if path != "/path/to/project/test/for/stuff/my-new-test" {
-			t.Fatalf("Failed to resolve path: %v", path)
+func TestResolveImportPath(t *testing.T) {
+	tsConfig := `{
+		"compilerOptions": {
+			"paths": {
+				"src/*": ["./src/*"],
+				"@testing/*": ["./test/for/stuff/*"]
+			}
 		}
-	})
-	t.Run("resolve local paths", func(t *testing.T) {
-		path := tsPathResolver.ResolveImportPath("./src/my-new-test")
-		if path != "/path/to/project/src/my-new-test" {
-			t.Fatalf("Failed to resolve path: %v", path)
-		}
-	})
+	}`
+
+	tests := []struct {
+		name       string
+		importPath string
+		expected   string
+	}{
+		{"resolve @paths", "@testing/my-new-test", "/path/to/project/test/for/stuff/my-new-test.ts"},
+		{"resolve local paths", "./src/my-new-test", "/path/to/project/src/my-new-test.ts"},
+	}
+
+	tsPathResolver, err := pathresolver.NewTsPathResolver([]byte(tsConfig), "/path/to/project")
+	if err != nil {
+		t.Fatalf("Failed to create ts path resolver: %v", err)
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tsPathResolver.ResolveImportPath(tt.importPath); got != tt.expected {
+				t.Errorf("ResolveImportPath() = %v, want %v", got, tt.expected)
+			}
+		})
+	}
 }
