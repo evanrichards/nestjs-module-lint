bd076c4536333cf7110e2acac54ab252821d6dc8
Evan Richards <tevanrichards@gmail.com>
2024-05-14 23:32:17 -0700

    add return type

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,9 +3,10 @@ package app
 import (
 	"context"
 	"errors"
-	"log"
+	"fmt"
 	"os"
 	"path/filepath"
+	"sync"
 
 	"github.com/loop-payments/nestjs-module-lint/internal/parser"
 	pathresolver "github.com/loop-payments/nestjs-module-lint/internal/path-resolver"
@@ -25,49 +26,120 @@ func init() {
 	lang = typescript.GetLanguage()
 }
 
-func Run(
+func RunForDirRecursively(
+	root string,
+) ([]*ModuleReport, error) {
+	info, err := os.Stat(root)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to access path: %w", err)
+	}
+
+	var files []string
+	if info.IsDir() {
+		files, err = FindTSFiles(root)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to find TypeScript files: %w", err)
+		}
+	} else {
+		files = []string{root}
+	}
+	var wg sync.WaitGroup
+	resultChan := make(chan struct {
+		*ModuleReport
+		error
+	})
+	for _, file := range files {
+		wg.Add(1)
+		go func(file string) {
+			defer wg.Done()
+			moduleReports, err := RunForModuleFile(file)
+			if err != nil {
+				resultChan <- struct {
+					*ModuleReport
+					error
+				}{nil, fmt.Errorf("failed to run app for %s: %w", file, err)}
+			}
+			for _, report := range moduleReports {
+				resultChan <- struct {
+					*ModuleReport
+					error
+				}{report, nil}
+			}
+		}(file)
+	}
+
+	// Close the result channel once all goroutines have completed
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	var results []*ModuleReport
+	for result := range resultChan {
+		if result.error != nil {
+			return nil, result.error
+		}
+		results = append(results, result.ModuleReport)
+	}
+
+	return results, nil
+}
+
+func RunForModuleFile(
 	pathToModule string,
-) error {
+) ([]*ModuleReport, error) {
 	qualifiedPathToModule := filepath.Join(cwd, pathToModule)
 
 	pathResolver, err := pathresolver.NewTsPathResolverFromPath(cwd)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	sourceCode, err := os.ReadFile(qualifiedPathToModule)
 	if err != nil {
-		return errors.Join(errors.New("could not read the input file, does it exist?"), err)
+		return nil, errors.Join(errors.New("could not read the input file, does it exist?"), err)
 	}
 	n, err := sitter.ParseCtx(context.Background(), sourceCode, lang)
 	if err != nil {
-		return errors.Join(errors.New("could not parse the input file, is it valid typescript?"), err)
+		return nil, errors.Join(errors.New("could not parse the input file, is it valid typescript?"), err)
 	}
 	importsByModule, err := parser.GetImportsByModuleFromFile(n, sourceCode)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	fileImports, err := getFileImports(n, sourceCode, pathResolver, qualifiedPathToModule)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	providerControllersByModule, err := parser.GetProviderControllersByModuleFromFile(n, sourceCode)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
+	moduleReports := make([]*ModuleReport, 0)
 	for module, imports := range importsByModule {
 		providerControllers, ok := providerControllersByModule[module]
 		if !ok {
-			log.Printf("No provider controllers found for module %s, all imports are unnecessary\n", module)
+			moduleReports = append(moduleReports, &ModuleReport{
+				ModuleName:         module,
+				Path:               qualifiedPathToModule,
+				UnnecessaryImports: imports,
+			})
 			continue
 		}
 
-		err := runForModule(module, imports, providerControllers, fileImports, pathResolver)
+		moduleReport, err := runForModule(module, imports, providerControllers, fileImports, pathResolver, qualifiedPathToModule)
 		if err != nil {
-			return err
+			return nil, err
 		}
+		moduleReports = append(moduleReports, moduleReport)
 	}
-	return nil
+	return moduleReports, nil
+}
+
+type ModuleReport struct {
+	ModuleName         string
+	Path               string
+	UnnecessaryImports []string
 }
 
 func runForModule(
@@ -76,17 +148,16 @@ func runForModule(
 	providerControllers []string,
 	fileImports []FileImportNode,
 	pathResolver *pathresolver.TsPathResolver,
-) error {
+	qualifiedPathToModule string,
+) (*ModuleReport, error) {
 	moduleNode := NewModuleNode(moduleName, importNames, providerControllers, fileImports, pathResolver)
 	unecessaryInputs, err := moduleNode.Check()
 	if err != nil {
-		return err
+		return nil, err
 	}
-	if len(unecessaryInputs) > 0 {
-		log.Printf("%d of %d imports are unnecessary for module %s\n", len(unecessaryInputs), len(importNames), moduleName)
-		for _, unecessaryInput := range unecessaryInputs {
-			log.Printf(" - %s\n", unecessaryInput)
-		}
-	}
-	return nil
+	return &ModuleReport{
+		ModuleName:         moduleName,
+		Path:               qualifiedPathToModule,
+		UnnecessaryImports: unecessaryInputs,
+	}, nil
 }
--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sync"
 
+	"encoding/json"
 	"github.com/loop-payments/nestjs-module-lint/internal/app"
 )
 
@@ -14,45 +14,13 @@ func main() {
 		log.Fatal("Usage: nestjs-module-lint <path-to-directory-or-file>")
 	}
 	inputPath := os.Args[1]
-
-	info, err := os.Stat(inputPath)
+	result, err := app.RunForDirRecursively(inputPath)
 	if err != nil {
-		log.Fatalf("Failed to access path: %v", err)
+		log.Fatal(err)
+	}
+	for _, moduleReport := range result {
+		data, _ := json.Marshal(moduleReport)
+		fmt.Println(string(data))
 	}
 
-	var files []string
-	if info.IsDir() {
-		files, err = app.FindTSFiles(inputPath)
-		if err != nil {
-			log.Fatalf("Failed to find TypeScript files: %v", err)
-		}
-	} else {
-		files = []string{inputPath}
-	}
-
-	var wg sync.WaitGroup
-	errChan := make(chan error, len(files))
-
-	for _, file := range files {
-		wg.Add(1)
-		go func(file string) {
-			defer wg.Done()
-			err := app.Run(file)
-			if err != nil {
-				errChan <- fmt.Errorf("failed to run app for %s: %w", file, err)
-				log.Printf("Failed to parse %s: %v", file, err)
-			}
-		}(file)
-	}
-
-	// Wait for all goroutines to finish
-	wg.Wait()
-	close(errChan)
-
-	// Check if there were any errors
-	for err := range errChan {
-		if err != nil {
-			log.Printf("Error encountered: %v", err)
-		}
-	}
 }
