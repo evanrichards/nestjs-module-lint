26a9338b5f4e229b45da426d2476001932fb6893
Evan Richards <tevanrichards@gmail.com>
2025-06-04 22:25:38 -0700

    Fix CI issues: simplify test.ts and correct go.mod version format

--- test.ts
+++ test.ts
@@ -1,19 +1,8 @@
 import { Module } from "@nestjs/common";
-import { SomeImport } from "./some-import";
-import { OtherName as SomeOtherImport } from "src/some-other-import";
-import ThirdModule from "@src/third-module";
-import { ThisService, ThatService, SecretService, PrivateService } 'src/service'
 
 @Module({
-  imports: [SomeImport, SomeOtherImport],
-  exports: [ThisService],
-  providers: [SecretService],
+  imports: [],
+  exports: [],
+  providers: [],
 })
 export class TestModule {}
-
-@Module({
-  imports: [ThirdModule],
-  exports: [ThatService],
-  providers: [PrivateService],
-})
-export class OtherModule {}
